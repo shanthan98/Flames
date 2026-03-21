dd0e4dec3c25e04bb8e7d21f28690a48bf71be86
Shanthan_Kasula <119076194+shanthan98@users.noreply.github.com>
2026-03-21 18:02:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -144,7 +144,7 @@ with left_col:
 with right_col:
     st.markdown("""
     <div style="
-        margin-top: 40px;
+        margin-top: 35px;
         background: rgba(0, 0, 0, 0.65);
         padding: 20px;
         border-radius: 15px;
