1a015760d724fd86e1477b9b0f492710052b3fd6
Shanthan_Kasula <119076194+shanthan98@users.noreply.github.com>
2026-03-21 17:16:59 -0500

    Update sheets.py

--- sheets.py
+++ sheets.py
@@ -1,27 +1,27 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime
+import streamlit as st
 
-# Connect to Google Sheets
 def connect_sheet():
+    creds_dict = st.secrets["gcp_service_account"]
+
     scope = [
         "https://spreadsheets.google.com/feeds",
         "https://www.googleapis.com/auth/drive"
     ]
-    
-    creds = ServiceAccountCredentials.from_json_keyfile_name(
-        "flames-credentials.json", scope
-    )
-    
+
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+
     client = gspread.authorize(creds)
     sheet = client.open("FlamesCalc_Project_Data").sheet1
-    
+
     return sheet
 
 
 def save_to_sheet(data):
     sheet = connect_sheet()
-    
+
     sheet.append_row([
         data["name1"],
         data["name2"],
